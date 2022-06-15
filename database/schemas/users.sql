a618ec678c49ed801fa6b2c66b48efc7558f361d
Tomasz Bortacki <tomasz.bortacki99@gmail.com>
2022-06-16 00:28:40 +0200

    Create user database structure

---
+++ database/schemas/users.sql
@@ -0,0 +1,19 @@
+create table public.users
+(
+    id_user    serial
+        constraint users_pk
+            primary key,
+    email      varchar not null,
+    password   varchar not null,
+    firstname  varchar,
+    lastname   varchar,
+    birthday   date,
+    permission integer not null
+);
+
+alter table public.users
+    owner to root;
+
+create unique index users_id_user_uindex
+    on public.users (id_user);
+
