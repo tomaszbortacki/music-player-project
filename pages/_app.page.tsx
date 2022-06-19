47c1c7a28bef39339c9f97b285459698739ad790
Tomasz Bortacki <tomasz.bortacki99@gmail.com>
2022-06-19 20:26:15 +0200

    Revert "Change progress bar"
    This reverts commit 1004884d60e8aa2e71d6172284bf78b39dd52dec.

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,19 +1,14 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
-import NextProgress from "next-progress";
+import NextNProgress from "nextjs-progressbar";
 
 import "react-toastify/dist/ReactToastify.min.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <NextProgress
-        height={4}
-        color={"#27AE60"}
-        delay={300}
-        options={{ showSpinner: false }}
-      />
+      <NextNProgress height={3} color="#27AE60" />
       <Component {...pageProps} />
       <ToastContainer />
     </>
